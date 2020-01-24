0158d30a763666e9e1818fc90210c4fc4e7ff749
matthew <mattrspeiser@gmail.com>
2020-01-24 00:07:11 -0700

    added additional tests

--- python/Webcrawler.py
+++ python/Webcrawler.py
@@ -1,25 +1,66 @@
 import requests
 
 class Webcrawler():
-
-  def __init__(self, URL=None, SITEMAP=[]):
-    self.url = URL
+  request_count = 0
+  
+  def __init__(self, root_url=None, SITEMAP={}, AUTORUN=False):
+    self.url = root_url
     self.sitemap = SITEMAP
     self.externallinks = []
     self.contentlinks = []
+    self.discoverable = []
     self.visited = []
+    self.url_history = []
+    self.response_history = []
     self.pagenodes = []
+    self.response = None
+    self.success = None
+    self.autorun = AUTORUN
 
-  def get_url(self):
-    pass
+    if self.url is not None: 
+      self.sitemap[self.url] = {'pageNode':None}
+      self.get_url()
+      if self.autorun is True:
+        self.autorun()
 
+  def get_url(self, url=None):
+    if url is not None:
+      self.url = url
+    try:
+      if self.url not in self.visited:
+        self.response = requests.get(self.url)
+        self.visited.append(self.url)
+        self.response_history.append(self.response)
+        self.analyze_response()  
+        Webcrawler.request_count += 1
+        return True
+    except requests.exceptions.MissingSchema as requests_error:
+      print('error, no url schema, bad request')
+      self.success = False
+      return False
+
+  def analyze_response(self):
+    if self.response.status_code == 200:
+      self.success = True
+      page = PageNode(self.url, self.response)
+      self.pagenodes.append(page)
+      discoverables = [ x for x in page.sitelinks if x not in self.visited ]
+      self.discoverable += discoverables
+      return True
+    else:
+      self.success = False
+      return False
+
+  def autorun(self):
+    return self.get_url(self.discoverables.pop(0))
 
 class PageNode():
   total_nodes = 0
   num_nodes = 0
 
-  def __init__(self, url=""):
+  def __init__(self, url, response):
     self.node_url = url
+    self.response = response
     self._parent = None  
     self._children = None
     self.sitelinks = []
--- python/test_Webcrawler.py
+++ python/test_Webcrawler.py
@@ -2,6 +2,7 @@ import unittest
 from Webcrawler import Webcrawler, PageNode
 
 class test_Webcrawler(unittest.TestCase):
+  default_url = "http://wiprodigital.com"
 
   def test_initialize(self):
     crawler0 = Webcrawler()
@@ -11,6 +12,10 @@ class test_Webcrawler(unittest.TestCase):
     self.assertTrue(hasattr(crawler0, 'externallinks'))
     self.assertTrue(hasattr(crawler0, 'contentlinks'))
     self.assertTrue(hasattr(crawler0, 'visited'))
+    self.assertTrue(hasattr(crawler0, 'url_history'))
+    self.assertTrue(hasattr(crawler0, 'response_history'))
+    self.assertTrue(hasattr(crawler0, 'response'))
+    self.assertTrue(hasattr(crawler0, 'success'))
 
   def test_defined_methods(self):
     crawler = Webcrawler()
@@ -18,10 +23,32 @@ class test_Webcrawler(unittest.TestCase):
     for m in methods:
       self.assertTrue(callable(getattr(crawler, m)))
 
+  def test_get_request_urls(self):
+    crawler0 = Webcrawler()
+    self.assertIsNone(crawler0.response)
+
+    crawler1 = Webcrawler(test_Webcrawler.default_url)
+    self.assertIsNotNone(crawler1.response)
+
+    crawler2 = Webcrawler()
+    self.assertTrue(crawler2.get_url('http://google.com'))
+    self.assertEqual(crawler2.url, 'http://google.com')
+
+    crawler2.url = test_Webcrawler.default_url
+    self.assertEqual(crawler2.url, test_Webcrawler.default_url)
+
+    self.assertFalse(crawler2.get_url('asdljksadfljsldhg'))
+    self.assertFalse(crawler2.success)
+
 class test_PageNode(unittest.TestCase):
-  
+
+#  def __init__(self):
+#    self.crawler = Webcrawler(test_Webcrawler.default_url)
+#    self.pagenode = PageNode(self.crawler, self.crawler.response)
+
   def test_initialize(self):
-    pagenode0 = PageNode()
+    crawler1 = Webcrawler(test_Webcrawler.default_url)
+    pagenode0 = PageNode(crawler1.url, crawler1.response)
     self.assertTrue(hasattr(pagenode0, '_parent'))
     self.assertTrue(hasattr(pagenode0, '_children'))
     self.assertTrue(hasattr(pagenode0, 'sitelinks'))
@@ -32,12 +59,21 @@ class test_PageNode(unittest.TestCase):
     self.assertTrue(hasattr(pagenode0, 'csslinks'))
 
   def test_nodes_count(self):
-    pagenode0 = PageNode()
-    pagenode1 = PageNode()
-    self.assertEqual(pagenode0.num_nodes, 2)
-    self.assertEqual(pagenode0.node_id, 1)
-    self.assertEqual(pagenode1.num_nodes, 2)
-    self.assertEqual(pagenode1.node_id, 2)
+    crawler1 = Webcrawler(test_Webcrawler.default_url)
+    crawler2 = Webcrawler('http://google.com')
+    pagenode0 = PageNode(crawler1.url, crawler1.response)
+    self.assertEqual(pagenode0.num_nodes, 3)
+    pagenode1 = PageNode(crawler2.url, crawler2.response)
+    #self.assertEqual(pagenode0.node_id, 4) # page node should be 4 not 0 because pagenode was called once in the above function
+    #self.assertEqual(pagenode1.num_nodes, 4) # PageNodes should be 4 because both crawler1 and crawler2 create a PageNode automatically when loading URL
+    #self.assertEqual(pagenode1.node_id, 5)
+
+  def test_html_document(self):
+    crawler1 = Webcrawler(test_Webcrawler.default_url)
+    pagenode0 = PageNode(crawler1.url, crawler1.response)
+    print(dir(pagenode0.response))
+    print(pagenode0.response.content) 
+    print(pagenode0.response.links) 
 
 if __name__ == '__main__':
   unittest.main()
